2ad052282da845a9a506512d7690a7e48a534093
James Sinkala <xinnks@gmail.com>
2023-02-07 20:30:18 +0300

    improvement: Update component with Options API logic and some styles

--- App.vue
+++ App.vue
@@ -1,5 +1,37 @@
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    },
+    computed: {
+        doubleL() {
+            return 1 + '1';
+        }
+    },
+    methods: {
+        increment(){
+            this.count++
+        }
+    }
+}
+</script>
+
 <template>
     <div>
-        He{{ 1 + ‘1’ }}o World!
+        He{{ doubleL }}o World!
+        <button @click="increment()">Clicks {{ count }}</button>
     </div>
 </template>
+
+<style scoped>
+div, button{
+    padding: 5px;
+    margin: 5px
+}
+button{
+    color: red;
+    font-weight: 800;
+}
+</style>
